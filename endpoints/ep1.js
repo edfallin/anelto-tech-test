9b91a476f9c71a9188aaaf920d53a55e8655cd9c
Ed Fallin <edfallin@earthlink.net>
2023-11-08 17:47:18 -0800

    Basically working concurrent-ish trivial endpoint listeners.  Not yet inter-communicating, though.

--- endpoints/ep1.js
+++ endpoints/ep1.js
@@ -2,71 +2,20 @@
 
 /* Express app to try simultaneous node instances talking to each other. */
 
-/**/
-
-/* Express app to try simultaneous node instances talking to each other. */
-
 let express = require("express");
-let https = require("https");
-let debug = require("debug")("ep1:server");
-
-let other = "https://localhost:32223";
-
 let app = express();
 
+let other = "http://localhost:32223";
 
 let port = 31113;
 app.set("port", port);
 app.use(express.json());
 
 
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
     console.log(`cruft : req.originalUrl:`, req.originalUrl);
+    res.send("reached");
 });
 
-let server = https.createServer(app);
-
-server.listen();
-server.on("error", whenErrorRaised);
-server.on("listening", whenListeningStarts);
-
-console.log(`cruft : end of run script`);
-
-// region Server event listeners
-
-function whenErrorRaised(error) {
-    if (error.syscall !== "listen") {
-        throw error;
-    }
-
-    let bind = typeof port === "string"
-        ? "Pipe " + port
-        : "Port " + port;
-
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case "EACCES":
-            console.error(bind + " requires elevated privileges");
-            process.exit(1);
-            break;
-        case "EADDRINUSE":
-            console.error(bind + " is already in use");
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
-
-function whenListeningStarts() {
-    console.log(`cruft : listener run`);
-    let addr = server.address();
-    console.log(`cruft : addr:`, addr);
-    let bind = typeof addr === "string"
-        ? "pipe " + addr
-        : "port " + addr.port;
-    debug("Listening on " + bind);
-}
-
-// endregion Server event listeners
+app.listen(port);
 
--- endpoints/ep2.js
+++ endpoints/ep2.js
@@ -3,66 +3,19 @@
 /* Express app to try simultaneous node instances talking to each other. */
 
 let express = require("express");
-let https = require("https");
-let debug = require("debug")("ep2:server");
-
-let other = "https://localhost:31113";
-
 let app = express();
 
 
-let port = 32223;
-app.set("port", port);
-app.use(express.json());
-
+let other = "https://localhost:31113";
 
 app.get("/", (req, res, next) => {
     console.log(`cruft : req.originalUrl:`, req.originalUrl);
+    res.send("It worked.");
 });
 
-let server = https.createServer(app);
 
-server.listen();
-server.on("error", whenErrorRaised);
-server.on("listening", whenListeningStarts);
+let port = 32223;
 
-console.log(`cruft : end of run script`);
+app.listen(port);
 
-// region Server event listeners
-
-function whenErrorRaised(error) {
-    if (error.syscall !== "listen") {
-        throw error;
-    }
-
-    let bind = typeof port === "string"
-        ? "Pipe " + port
-        : "Port " + port;
-
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case "EACCES":
-            console.error(bind + " requires elevated privileges");
-            process.exit(1);
-            break;
-        case "EADDRINUSE":
-            console.error(bind + " is already in use");
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
-
-function whenListeningStarts() {
-    console.log(`cruft : listener run`);
-    let addr = server.address();
-    console.log(`cruft : addr:`, addr);
-    let bind = typeof addr === "string"
-        ? "pipe " + addr
-        : "port " + addr.port;
-    debug("Listening on " + bind);
-}
-
-// endregion Server event listeners
 
