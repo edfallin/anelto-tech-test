90b5c1551c13d1986bb470fe311750be4329bcb8
Ed Fallin <edfallin@earthlink.net>
2023-11-08 17:58:22 -0800

    More iffy experiments.

--- endpoints/ep1.js
+++ endpoints/ep1.js
@@ -8,14 +8,18 @@ let app = express();
 let other = "http://localhost:32223";
 
 let port = 31113;
-app.set("port", port);
-app.use(express.json());
-
 
 app.get("/", (req, res) => {
     console.log(`cruft : req.originalUrl:`, req.originalUrl);
-    res.send("reached");
+    fetcher().then(text => res.send("reached" + text));
 });
 
 app.listen(port);
 
+
+
+async function fetcher() {
+    let response = await fetch(other);
+    let result = await response.text();
+    return result;
+}
