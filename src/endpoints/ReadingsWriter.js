170bd888d7df15d52c1e3d7e24269088eefe1864
Ed Fallin <edfallin@earthlink.net>
2023-11-11 11:10:21 -0800

    Simplified to all-ESM approach with fewer elements.

--- src/endpoints/ReadingsWriter.js
+++ src/endpoints/ReadingsWriter.js
@@ -3,22 +3,69 @@
 /* External endpoint for patient data. */
 
 import express from "express";
-import { Writer } from "../readings/Writer.js";
+// import { Writer } from "../readings/Writer.js";
+import { Storage } from "../storage/Storage.js";
 
-let app = express();
-app.use(express.json());
+// let app = express();
+// app.use(express.json());
+
+// let service = new Writer();
+
+// app.post("/add/", (req, res) => {
+//     let outcome = service.storeReading(req.body);
+//     res.ok = true;
+//     res.send("Added");
+// });
+
+// app.listen(31001);
 
 
-let service = new Writer();
-// let source = import("../src/readings/Writer.js")
-//     .then(source => service = new source.Writer());
+/* micro to handle all writing (storing) of data (by devices) */
 
+/* details perhaps partly based on Express routers */
 
-app.post("/add/", (req, res) => {
-    let outcome = service.storeReading(req.body);
-    res.ok = true;
-    res.send("Added");
-});
+export class Writer {
+    #port = 31001;
+    #app;
+    
+    constructor() {
+        this.initHttp();
+        this.initExternalListeners();
+    }
 
+    initHttp() {
+        this.#app = express();
+        this.#app.use(express.json());
+    }
 
-app.listen(31001);
+    initExternalListeners() {
+        this.#app.post("/add/", (req, res) => {
+            let outcome = this.storeReading(req.body);
+            res.ok = true;
+            res.send("Added");
+        });
+
+        // registers self to pub-sub with HTTP fetch
+    }
+
+    run() {
+        this.#app.listen(this.#port);
+    }
+
+    storeReading(json) {
+        console.log(`cruft : json:`, json);
+        
+        if (json.systolic - json.diastolic > 50) {
+            return "Range is wide";
+        }
+
+        return "Range is narrow";
+    }
+
+    isValidReading(json) {
+    }
+
+}
+
+let writer = new Writer();
+writer.run();
