be3d33641a0383d2a775d3c45179850ae669798c
Ed Fallin <edfallin@earthlink.net>
2023-11-12 19:55:46 -0800

    Adding file for some unit tests.

---
+++ tests/WatcherTests.rt.js
@@ -0,0 +1,10 @@
+/**/
+
+import { ATestSource } from "risei/ATestSource";
+import Watcher from "../src/endpoints/Watcher.js";
+
+export class WatcherTests extends ATestSource {
+    tests = [
+    ];
+}
+
